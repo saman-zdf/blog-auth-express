3d49176cde7b57ed8027fe5b7b0393837771e43e
saman-zdf <samanfyz68@gmail.com>
2021-12-06 17:16:08 +0800

    login controller has been added

--- routes/auth.js
+++ routes/auth.js
@@ -23,4 +23,18 @@ router.post('/auth/register', async (req, res) => {
   }
 });
 
+// login
+router.post('/auth/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json('Wrong credentials!');
+    const validate = await bcrypt.compare(req.body.password, user.password);
+    !validate && res.status(400).json('Wrong credentials!');
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
 module.exports = router;
