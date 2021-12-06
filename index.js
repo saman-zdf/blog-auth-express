965861719c729762e5738292443f9428b7c61e29
saman-zdf <samanfyz68@gmail.com>
2021-12-06 17:02:38 +0800

    auth controller been added, register function with bcrypt has been added

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+// middleware
+app.use(express.json());
+
 // connecting mongoDB
 mongoose
   .connect(process.env.DATABASE_URI)
   .then(() => console.log('connected to mongo'))
   .catch((err) => console.log(err));
+
+// import auth router
+const authRouter = require('./routes/auth');
+app.use('/api/v1', authRouter);
 
 app.listen(5000, () => {
   console.log('server is on port 5000');
