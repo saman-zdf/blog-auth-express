d0621ca51f71e572636ced3f5316762c9ac5d0db
saman-zdf <samanfyz68@gmail.com>
2021-12-06 16:36:16 +0800

    models been added

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// connecting mongoDB
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(() => console.log('connected to mongo'))
+  .catch((err) => console.log(err));
 
 app.listen(5000, () => {
   console.log('server is on port 5000');
